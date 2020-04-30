1398d73a135fd0c645a7d65c1f4969ccf75d9fc1
Lalit <lalit.vijay952@gmail.com>
2020-04-30 14:06:02 +0530

    Cleaned up App.js with view components

--- src/App.js
+++ src/App.js
@@ -1,98 +1,27 @@
 import React, {Component} from 'react';
-import { Statistic, Card, Row, Col, Layout, Breadcrumb } from 'antd';
+import { Layout } from 'antd';
 import { Line } from '@antv/g2plot';
 import "antd/dist/antd.css";
 import './App.css';
-const { Header, Content, Footer } = Layout;
+import HeaderLayout from './components/header';
+import GraphLayout from './components/graphLayout';
+import FooterLayout from './components/footer';
 
 //TODO Put things in components 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Layout>
-    <Header className="header">
-      <div className="headerContent" />
-      <div className="text-white bold font-24"><img className="logoHolder" src="/logo192.png" alt="Tweetonomics logo"/>
-      Tweetonomics
+        <Layout>
+          <HeaderLayout></HeaderLayout>
+          <Layout>
+            <Layout className="text-center bold body">
+              <GraphLayout></GraphLayout>
+              <FooterLayout></FooterLayout>
+            </Layout>
+          </Layout>
+        </Layout>
       </div>
-
-    </Header>
-    
-    <Layout>
-      
-      <Layout className="text-center bold body" style={{ padding: '0 24px 24px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          
-          <Breadcrumb.Item>Elon Musk</Breadcrumb.Item>
-          <Breadcrumb.Item>Tesla</Breadcrumb.Item>
-        </Breadcrumb>
-        <Content 
-          className="site-layout-background text-center fullHeight "
-          style={{
-            padding: 24,
-            margin: 0,
-            
-          }}
-          style={{minHeight:"56vh"}}
-        >
-        <br />
-          Corelation in Share price of Tesla and Tweet's of Elon Musk
-          <div id="canvas"></div>
-        </Content>
-        <Content>
-        <div className="site-statistic-demo-card">
-    <Row style={{paddingTop:"16px"}} gutter={16}>
-      <Col span={6}>
-        <Card>
-          <Statistic
-            title="Today Tweet count"
-            value={11}
-            valueStyle={{ color: '#3f8600' }}
-          />
-        </Card>
-      </Col>
-      <Col span={6}>
-        <Card>
-          <Statistic
-            title="Total Tweet count"
-            value={12345}
-            valueStyle={{ color: '#222222' }}
-          />
-        </Card>
-      </Col>
-      <Col span={6}>
-        <Card>
-          <Statistic
-            title="Market Cap"
-            value={109.32}
-            precision={2}
-            valueStyle={{ color: '#0779e4' }}
-            prefix="$"
-            suffix="B"
-          />
-        </Card>
-      </Col>
-      <Col span={6}>
-        <Card>
-          <Statistic
-            title="Share Price"
-            value={749.3}
-            precision={2}
-            valueStyle={{ color: '#cf1322' }}
-            prefix="$"
-            
-          />
-        </Card>
-      </Col>
-    </Row>
-  </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Tweetnomics ©2020 Created by <a href="#">Lalit Vijay</a></Footer>
-      </Layout>
-    </Layout>
-  </Layout>
-    </div>
     )
 }
 
